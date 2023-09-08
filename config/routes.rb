1e0b3b3a4af16f27d22e8914a9ae9fb66d555d9f
Lucas Lima <lucasrlima@gmail.com>
2023-09-08 19:17:35 +0100

    As a user, I can edit a task

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   post "tasks",    to: "tasks#create"
   
   get "tasks/:id", to: "tasks#show", as: :task
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task 
+  patch "tasks/:id", to: "tasks#update"
   
   # Defines the root path route ("/")
   # root "articles#index"
